bf19256c93ce91dd64ff304f76d3e9dcba1a6dc5
Foxie Flakey <foxieflakey@gmail.com>
2024-11-27 18:05:35 +0700

    Concurrency test

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,21 @@ fn main() {
   let manager = Arc::new(ObjectManager::new());
   
   let manager_for_thread = manager.clone();
-  thread::spawn(move || {
+  let join = thread::spawn(move || {
     let ctx = manager_for_thread.create_context();
     ctx.alloc(|| "Alive 1");
     ctx.alloc(|| 2 as u32);
     ctx.alloc(|| "Alive 2".to_string());
-    ctx.alloc(|| 1 as u32);
-    ctx.alloc(|| 2 as u32);
     drop(ctx);
-  }).join().unwrap();
+  });
   
+  let ctx = manager.create_context();
+  ctx.alloc(|| "Alive 1");
+  ctx.alloc(|| 2 as u32);
+  ctx.alloc(|| "Alive 2".to_string());
+  drop(ctx);
+  
+  join.join().unwrap();
   println!("Sweeping");
   
   // Keep non u32 data alive 
