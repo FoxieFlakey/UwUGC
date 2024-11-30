d19340b2e4e66ebf71f7b9acb34ae9b1a8bfbc5e
Foxie Flakey <foxieflakey@gmail.com>
2024-11-30 13:29:21 +0700

    mimalloc

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@
 #![feature(sync_unsafe_cell)]
 #![deny(unsafe_op_in_unsafe_fn)]
 
-use std::{hint::black_box, io::{self, Write}, sync::{atomic::Ordering, Arc}, thread::{self, JoinHandle}, time::{Duration, Instant}};
+use std::{ffi::{c_int, c_long}, hint::black_box, io::{self, Write}, sync::{atomic::Ordering, Arc}, thread::{self, JoinHandle}, time::{Duration, Instant}};
 
 use heap::Heap;
+use mimalloc::MiMalloc;
 use portable_atomic::AtomicBool;
 use util::data_collector::DataCollector;
 
@@ -16,6 +17,9 @@ mod gc;
 static QUIT_THREADS: AtomicBool = AtomicBool::new(false);
 const MAX_SIZE: usize = 512 * 1024 * 1024;
 const TRIGGER_SIZE: usize = 128 * 1024 * 1024;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
 
 fn start_gc_thread(heap: Arc<Heap>) -> JoinHandle<()> {
   return thread::spawn(move ||{
@@ -68,6 +72,38 @@ fn do_test<const LENGTH: usize>(input: &mut [i32; LENGTH]) -> &mut [i32; LENGTH]
 fn main() {
   println!("Hello, world!");
   
+  // Reserve 512 MiB from mimalloc
+  // mimalloc crate does not expose
+  // necessary mi_reserve_os_memory function for this
+  const MI_OPTION_SHOW_ERRORS: c_int = 0;
+  const MI_OPTION_VERBOSE: c_int = 2;
+  const MI_OPTION_ARENA_EAGER_COMMIT: c_int = 4;
+  const MI_OPTION_PURGE_DELAY: c_int = 15;
+  
+  extern "C" {
+    fn mi_reserve_os_memory(size: usize, commit: bool, allow_large: bool) -> c_int;
+    fn mi_option_set(option: c_int, val: c_long);
+    fn mi_option_enable(option: c_int);
+  }
+  
+  unsafe {
+    mi_option_enable(MI_OPTION_SHOW_ERRORS);
+    mi_option_enable(MI_OPTION_ARENA_EAGER_COMMIT);
+    mi_option_set(MI_OPTION_PURGE_DELAY, 30_000);
+  };
+  
+  if unsafe { mi_reserve_os_memory(512 * 1024 * 1024, true, false) } != 0 {
+    panic!("Error reserving memory");
+  }
+  
+  // Need to use the half a gig to actually commits it
+  {
+    // Alloc half a gig and zero it and dealloc it
+    let mut tmp =unsafe { black_box(Box::<[u8; 768 * 1024 * 1024]>::new_uninit().assume_init()) };
+    tmp.fill(0xFC);
+    println!("Prepared the memory!");
+  }
+  
   let heap = Arc::new(Heap::new());
   let stat_collector = Arc::new(DataCollector::new(4096));
   
@@ -80,24 +116,31 @@ fn main() {
     io::stdout().flush().unwrap();
   });
   
-  let gc_thread = start_gc_thread(heap.clone());
-  let stat_thread = start_stat_thread(heap.clone(), stat_collector.clone());
+  let gc_thread = {
+    if true {
+      Some(start_gc_thread(heap.clone()))
+    } else {
+      None
+    }
+  };
+  
+  let stat_thread = {
+    if !true {
+      Some(start_stat_thread(heap.clone(), stat_collector.clone()))
+    } else {
+      None
+    }
+  };
   
   let ctx = heap.create_context();
   
   // Raw is 1.5x faster than GC
   let start_time = Instant::now();
   let temp = [198; 1024];
-  black_box(for _ in 1..200_000 {
-    if true {
-      let mut res = ctx.alloc(|| temp);
-      black_box(do_test(res.borrow_inner_mut()));
-      black_box(res);
-    } else {
-      let mut res = black_box(Box::new(temp));
-      black_box(do_test(res.as_mut()));
-      black_box(Box::leak(res));
-    }
+  black_box(for _ in 1..400_000 {
+    let mut res = ctx.alloc(|| temp);
+    black_box(do_test(res.borrow_inner_mut()));
+    black_box(res);
   });
   drop(ctx);
   
@@ -107,8 +150,12 @@ fn main() {
   println!("");
   println!("Shutting down!");
   
-  gc_thread.join().unwrap();
-  stat_thread.join().unwrap();
+  if let Some(thrd) = gc_thread {
+    thrd.join().unwrap();
+  }
+  if let Some(thrd) = stat_thread {
+    thrd.join().unwrap();
+  }
   drop(stat_collector);
   
   println!("Test time was {complete_time:.2} secs");
