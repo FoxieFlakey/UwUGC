60b04ee212155f51f42cd604bf2bb81d5c394909
Foxie Flakey <foxieflakey@gmail.com>
2024-12-21 20:31:22 +0700

    Move non_miri to its own module

--- uwugc-test/src/main.rs
+++ uwugc-test/src/main.rs
@@ -1,8 +1,7 @@
 #![deny(unsafe_op_in_unsafe_fn)]
 
-use std::{ffi::{c_int, c_long}, hint::black_box, io::{self, Write}, mem::offset_of, sync::{atomic::Ordering, Arc, LazyLock}, thread::{self, JoinHandle}, time::{Duration, Instant}};
+use std::{hint::black_box, io::{self, Write}, mem::offset_of, sync::{atomic::Ordering, Arc, LazyLock}, thread::{self, JoinHandle}, time::{Duration, Instant}};
 
-use mimalloc::MiMalloc;
 use std::sync::atomic::AtomicBool;
 use data_collector::DataCollector;
 
@@ -16,8 +15,7 @@ const POLL_RATE: u64 = 20;
 const TRIGGER_SIZE: usize = 64 * 1024 * 1024;
 
 #[cfg(not(miri))]
-#[global_allocator]
-static GLOBAL: MiMalloc = MiMalloc;
+mod non_miri;
 
 fn start_stat_thread(heap: HeapArc, stat_collector: Arc<DataCollector<HeapStatRecord>>) -> JoinHandle<()> {
   return thread::spawn(move || {
@@ -49,48 +47,10 @@ fn do_test<const LENGTH: usize>(input: &mut [i32; LENGTH]) -> &mut [i32; LENGTH]
   return input;
 }
 
-fn prepare_mimalloc() {
-  // Reserve 512 MiB from mimalloc
-  // mimalloc crate does not expose
-  // necessary mi_reserve_os_memory function for this
-  #[allow(dead_code)]
-  const MI_OPTION_SHOW_ERRORS: c_int = 0;
-  #[allow(dead_code)]
-  const MI_OPTION_VERBOSE: c_int = 2;
-  #[allow(dead_code)]
-  const MI_OPTION_ARENA_EAGER_COMMIT: c_int = 4;
-  #[allow(dead_code)]
-  const MI_OPTION_PURGE_DELAY: c_int = 15;
-  
-  extern "C" {
-    fn mi_reserve_os_memory(size: usize, commit: bool, allow_large: bool) -> c_int;
-    fn mi_option_set(option: c_int, val: c_long);
-    fn mi_option_enable(option: c_int);
-  }
-  
-  unsafe {
-    mi_option_enable(MI_OPTION_SHOW_ERRORS);
-    mi_option_enable(MI_OPTION_ARENA_EAGER_COMMIT);
-    mi_option_set(MI_OPTION_PURGE_DELAY, 30_000);
-  };
-  
-  if unsafe { mi_reserve_os_memory(512 * 1024 * 1024, true, false) } != 0 {
-    panic!("Error reserving memory");
-  }
-  
-  // Need to use the half a gig to actually commits it
-  if !true {
-    // Alloc half a gig and zero it and dealloc it
-    let mut tmp =unsafe { black_box(Box::<[u8; 768 * 1024 * 1024]>::new_uninit().assume_init()) };
-    tmp.fill(0xFC);
-    println!("Prepared the memory!");
-  }
-}
-
 fn main() {
   println!("Hello, world!");
   #[cfg(not(miri))]
-  prepare_mimalloc();
+  non_miri::prepare_mimalloc();
   
   let heap = HeapArc::new(HeapParams {
     gc_params: GCParams {
---
+++ uwugc-test/src/non_miri.rs
@@ -0,0 +1,44 @@
+use std::{ffi::{c_int, c_long}, hint::black_box};
+
+use mimalloc::MiMalloc;
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
+pub fn prepare_mimalloc() {
+  // Reserve 512 MiB from mimalloc
+  // mimalloc crate does not expose
+  // necessary mi_reserve_os_memory function for this
+  #[allow(dead_code)]
+  const MI_OPTION_SHOW_ERRORS: c_int = 0;
+  #[allow(dead_code)]
+  const MI_OPTION_VERBOSE: c_int = 2;
+  #[allow(dead_code)]
+  const MI_OPTION_ARENA_EAGER_COMMIT: c_int = 4;
+  #[allow(dead_code)]
+  const MI_OPTION_PURGE_DELAY: c_int = 15;
+  
+  extern "C" {
+    fn mi_reserve_os_memory(size: usize, commit: bool, allow_large: bool) -> c_int;
+    fn mi_option_set(option: c_int, val: c_long);
+    fn mi_option_enable(option: c_int);
+  }
+  
+  unsafe {
+    mi_option_enable(MI_OPTION_SHOW_ERRORS);
+    mi_option_enable(MI_OPTION_ARENA_EAGER_COMMIT);
+    mi_option_set(MI_OPTION_PURGE_DELAY, 30_000);
+  };
+  
+  if unsafe { mi_reserve_os_memory(512 * 1024 * 1024, true, false) } != 0 {
+    panic!("Error reserving memory");
+  }
+  
+  // Need to use the half a gig to actually commits it
+  if !true {
+    // Alloc half a gig and zero it and dealloc it
+    let mut tmp =unsafe { black_box(Box::<[u8; 768 * 1024 * 1024]>::new_uninit().assume_init()) };
+    tmp.fill(0xFC);
+    println!("Prepared the memory!");
+  }
+}
