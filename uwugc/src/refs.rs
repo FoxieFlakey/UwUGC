635baefe236897fc333359f96a96ece5370ba96c
Foxie Flakey <foxieflakey@gmail.com>
2024-12-22 17:09:55 +0700

    Cleaning up codes in refs.rs after enabling every lint for clippy

--- uwugc/src/refs.rs
+++ uwugc/src/refs.rs
@@ -13,19 +13,19 @@ pub struct GCRefRaw<T: ObjectLikeTrait> {
 impl<T: ObjectLikeTrait> GCRefRaw<T> {
   pub fn new(data: *const Object) -> Self {
     return Self {
-      ptr: AtomicPtr::new(data as *mut Object),
+      ptr: AtomicPtr::new(data.cast_mut()),
       _phantom: PhantomData {}
     }
   }
   
   #[expect(dead_code)]
   pub unsafe fn store<'a>(&self, _ctx: &'a ContextHandle, root_ref: &RootRefRaw<'a, T>, _block_gc_cookie: &mut GCLockCookie) {
-    self.ptr.swap(root_ref.get_object_borrow() as *const Object as *mut Object, Ordering::Relaxed);
+    self.ptr.swap(ptr::from_ref(root_ref.get_object_borrow()).cast_mut(), Ordering::Relaxed);
   }
   
   pub fn load<'a>(&self, ctx: &'a ContextHandle, block_gc_cookie: &mut GCLockCookie) -> Option<RootRefRaw<'a, T>> {
     let ptr = self.ptr.load(Ordering::Relaxed);
-    if ptr == ptr::null_mut() {
+    if ptr.is_null() {
       return None;
     }
     
