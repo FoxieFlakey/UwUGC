0bbb6d6065a516b48e7e943336570102a064c574
Foxie Flakey <foxieflakey@gmail.com>
2024-12-22 18:49:26 +0700

    Enable almost all clippy lints

--- uwugc/src/lib.rs
+++ uwugc/src/lib.rs
@@ -1,3 +1,12 @@
+#![deny(clippy::perf)]
+#![deny(clippy::correctness)]
+#![deny(clippy::suspicious)]
+#![deny(clippy::pedantic)]
+#![deny(clippy::complexity)]
+#![deny(clippy::style)]
+
+#![allow(clippy::missing_safety_doc)]
+#![allow(clippy::needless_return)]
 #![feature(box_as_ptr)]
 
 mod api;
