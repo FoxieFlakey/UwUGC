b8fad65b572f0cd04b05bc3f4dc9f8ed58229037
Foxie Flakey <foxieflakey@gmail.com>
2024-12-22 22:52:47 +0700

    Ooops missed this needless return in macro OwO

--- uwugc/src/descriptor.rs
+++ uwugc/src/descriptor.rs
@@ -53,7 +53,7 @@ macro_rules! impl_for_trait {
   ($trait_name:ident) => {
     unsafe impl<T: $trait_name> Describeable for T {
       fn get_descriptor() -> Option<&'static Descriptor> {
-        return None;
+        None
       }
     }
   };
