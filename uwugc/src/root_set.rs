27e13e72aa7c78b0db80ec3f6d56b5cb8de2ac98
Foxie Flakey <foxieflakey@gmail.com>
2026-08-16 23:11:10 +0700

    accidentally removed reference for iter_pointers

--- uwugc/src/root_set.rs
+++ uwugc/src/root_set.rs
@@ -7,7 +7,7 @@ use crate::object::ObjectPtr;
 // GC else GC willl be unsafe
 pub unsafe trait RootSet: Send + Any {
     fn map_pointers(&mut self, visitor: &mut dyn FnMut(ObjectPtr) -> ObjectPtr);
-    fn iter_pointers(&self, visitor: &mut dyn FnMut(ObjectPtr));
+    fn iter_pointers(&self, visitor: &mut dyn FnMut(&ObjectPtr));
     fn clone_boxed(&self) -> Box<dyn RootSet>;
 }
 
